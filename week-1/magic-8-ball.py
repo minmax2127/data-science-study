e39b13dec4e41371c294d57a0fc5c3e6a7705cce
minmax2127 <mbernales2127@gmail.com>
2025-05-05 06:30:27 +0800

    Update: magic-8-ball.py

---
+++ week-1/magic-8-ball.py
@@ -0,0 +1,51 @@
+# Magic 8 Ball
+# Concepts focused on: Functions
+
+import random
+
+def shake_ball(numm) : 
+    if num == 0:
+        return 'Stop playing here. Do your schoolworks!'
+    elif num == 1:
+        return 'It is certain'
+    elif num == 2:
+        return 'It is decidedly so'
+    elif num == 3:
+        return 'Yes'
+    elif num == 4:
+        return 'Hazy reply, try again!'
+    elif num == 5:
+        return 'Ask again later'
+    elif num == 6:
+        return 'Concentrate and ask again'
+    elif num == 7:
+        return 'My reply is no'
+    elif num == 8:
+        return 'Outlook not looking so good'
+    elif num == 9:
+        return 'Very doubtful'
+    
+
+print("\nMagic 8 Ball!")
+
+
+while True:
+    question = input("\nAsk your question: ")
+    if question == 'stop':
+        break
+
+    # get the average of 10 results (for better chance)
+    avg = 0
+    for i in range(0, 9):
+        num = random.randint(0,9)
+        avg += num
+    avg = avg / 10
+
+    print(shake_ball(avg))
+
+    print("\n\tEnter 'stop' to end program.")
+    
+    
+
+print("\nHope I gave you clarity babes!")
+
